a7a7413383c17f925dbf7f9595232f65f1a0d238
Natatashkin <cs070283sng@gmail.com>
2022-10-04 18:29:43 +0300

    finish

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ const API_KEY = "14467768-9171c4f16b15a9d8391496270";
 
 export const getRandomImages = async () => {
   const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=all&page=1&per_page=3&safesearch=true&orientation=horizontal&image_type=photo`
+    `${BASE_URL}?key=${API_KEY}&q=all&page=1&per_page=21&safesearch=true&orientation=horizontal&image_type=photo`
   );
   const { hits } = await response.json();
   return hits;
